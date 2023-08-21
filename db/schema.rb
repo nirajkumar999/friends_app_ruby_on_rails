31b994c13e1368d936b1a782451a589323d6fbdd
niraj <1751nk@gmail.com>
2023-08-21 11:59:13 +0530

    continued

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_185911) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_061824) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_20_185911) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
